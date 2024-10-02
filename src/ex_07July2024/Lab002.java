65a4799d58cee5a8cdc2a69ae8f1cd6a2b5ba3cd
Shiksha-gahlyan <Shikshagahlyan108@gmail.com>
2024-10-03 00:09:32 +0530

    commit

---
+++ src/ex_07July2024/Lab002.java
@@ -0,0 +1,8 @@
+package ex_07July2024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello Shiksha");
+        System.out.println("Welcome!");
+    }
+}
